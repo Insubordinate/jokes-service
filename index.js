f78d407166d275a1e61affd96a6f7082b5da062f
Axl Cruz <acruz8344@gmail.com>
2023-04-24 15:46:47 -0400

    added stuff

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const {Op} = require('sequelize')
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    id = req.query.id ? req.query.id : null
+    content = req.query.joke ? req.query.joke : null
+    var jokes = [];
+
+    if (id && content) {
+      jokes = await Joke.findAll(
+        {
+          where: {
+            joke:{
+              [Op.substring]:content
+            },
+            id:id
+          }
+        }
+      )
+
+    } else if (id) {
+      jokes = await Joke.findAll(
+        {
+          where: {
+            id: id
+          }
+        })
+
+    } else if (content) {
+      jokes = await Joke.findAll(
+        {
+          where: {
+            joke:{
+              [Op.substring]:content
+            }
+          }
+        }
+      )
+    } else {
+      jokes = await Joke.findAll()
+    }
+
+
     res.send(jokes);
   } catch (error) {
     console.error(error);
